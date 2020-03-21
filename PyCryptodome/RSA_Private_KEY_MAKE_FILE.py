816c184e244c03c7354468bc2ffc15aee73f454e
bluetoonist <bluetoon@naver.com>
2020-03-22 00:08:38 +0900

    update repository python

--- PyCryptodome/RSA_Private_KEY_MAKE_FILE.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:utf-8 -*-
-from Crypto.PublicKey import RSA
-
-# PK_Key File Generate 
-
-private_key = RSA.generate(1024)
-f = open("./my.key" ,'wb+')
-f.write(private_key.exportKey('PEM'))
-f.close()
--- scapy/Scapy_Ex_1.py
+++
@@ -1,3 +0,0 @@
-from scapy.all import *
-
-sniff(prn=lambda x:print(x) ,count=1)
